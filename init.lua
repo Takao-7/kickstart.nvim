4e084bfa79cdd410229e7d610101aef6c907462f
Paul Larraß <takao@mailbox.org>
2025-01-14 13:47:51 +0100

    !U Switch header-source; Increased column width for "Document Symbols" window

--- init.lua
+++ init.lua
@@ -255,6 +255,14 @@ require('lazy').setup({
     dependencies = {
       'nvim-lua/plenary.nvim',
     },
+    config = function ()
+      require('ouroboros').setup(
+        {
+          switch_to_open_pane_if_possible = true,
+          vim.keymap.set('n', '<A-o>', ':Ouroboros<CR>')
+        }
+      )
+    end
   },
 
   {
@@ -556,7 +564,12 @@ require('lazy').setup({
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+          local builtin = require('telescope.builtin')
+          vim.keymap.set('n', '<leader>ds', function()
+            builtin.lsp_document_symbols({ show_line = true, symbol_width = 60 })
+            end,
+            { buffer = event.buf, desc = 'LSP: [D]ocument [S]ymbols'})
+          -- map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
 
           -- Fuzzy find all the symbols in your current workspace.
           --  Similar to document symbols, except searches over your entire project.
@@ -714,33 +727,6 @@ require('lazy').setup({
         end,
         mode = '',
         desc = '[F]ormat buffer',
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        local lsp_format_opt
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          lsp_format_opt = 'never'
-        else
-          lsp_format_opt = 'fallback'
-        end
-        return {
-          timeout_ms = 500,
-          lsp_format = lsp_format_opt,
-        }
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
     },
   },
